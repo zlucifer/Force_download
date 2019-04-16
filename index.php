e4d15d9ea586beb6f21be1b2bc6ce970fcea7cdd
zlucifer <dekxter000@gmail.com>
2019-04-16 19:21:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $file = 'nama_file_yang_ingin_dibuat_force_download.format_file'; // contoh: gam
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="'.basename($file).'"');
-header('Expires: 0');
+header('Expires: 0'); 
 header('Cache-Control: must-revalidate');
 header('Pragma: public');
 header('Content-Length: ' . filesize($file));
